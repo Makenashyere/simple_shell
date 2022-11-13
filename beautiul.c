04ba3f9a32d117cba8aa44bd3da2ca176e3b6263
Makena Hildah <makenabenedict@gmail.com>
2022-11-13 07:15:11 -0800

    start of the project

---
+++ beautiul.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - finds sum of 2 ints
+ * Return: 0
+ */
+int main (void)
+{
+	int i, j, sum;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 10; j > 0; j--)
+		{
+			sum = i + j;
+
+			return (sum);
+		}
+	}
+
+	return (0);
+}
+
